dfb0f5fa4cdfd777e953aeb17560eff7f8905139
Rakesh Nagapuri <Rakesh.Nagapuri@planonsoftware.com>
2023-06-29 23:01:05 +0530

    HeightOfTree

---
+++ improved/Trees/HeightOfTree.java
@@ -0,0 +1,51 @@
+/*
+You are given the root node of a binary tree A. You have to find the height of the given tree.
+
+A binary tree's height is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Input 1:
+
+ Values =  1 
+          / \     
+         4   3                        
+Input 2:
+
+ 
+ Values =  1      
+          / \     
+         4   3                       
+        /         
+       2
+
+Output 1:
+
+ 2 
+Output 2:
+
+ 3 
+
+*/
+
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) {
+ *      val = x;
+ *      left=null;
+ *      right=null;
+ *     }
+ * }
+ */
+public class Solution {
+    public int solve(TreeNode A) {
+        if(A==null){
+            return 0;
+        }
+        int lheight = solve(A.left);
+        int rheight = solve(A.right);
+        return Math.max(lheight,rheight)+1;
+    }
+}
