9aff2b5f3c51c1834794b11499c2d4aeecf71fc5
Rakesh Nagapuri <Rakesh.Nagapuri@planonsoftware.com>
2023-07-12 10:54:16 +0530

    Revision SortingTechniques

--- Revision/SortingTechniques.java
+++ Revision/SortingTechniques.java
@@ -8,6 +8,8 @@ public class Main {
        
         System.out.println(Arrays.toString(bubbleSort(A)));    
         System.out.println(Arrays.toString(insertionSort(A)));	
+		
+		System.out.println(Arrays.toString(mergeSort(A,0,A.length-1)));
         
     }	
 	public static int[] bubbleSort(int A[]){
@@ -35,7 +37,44 @@ public class Main {
         }
         return A;
 	}
-	
-	
+	public static int[] mergeSort(int A[],int low,int high){
+		if(low==high){
+			int sa[] = new int[1];
+			sa[0]=A[low];
+			return sa;
+		}
+		int mid=(low+high)/2;
+		int A1[] = mergeSort(A,low,mid);
+		int A2[] = mergeSort(A,mid+1,high);
+		return mergeTwoSortedArrays(A1,A2);
+	}
+	public static int[] mergeTwoSortedArrays(int A[],int B[]){
+		int n=A.length;
+		int m=B.length;
+		int i=0,j=0,k=0;
+		int ans[] = new int[n+m];
+		while(i<n && j<m){
+			if(A[i]<B[j]){
+				ans[k]=A[i];
+				i++;
+				k++;
+			}else{
+				ans[k]=B[j];
+				j++;
+				k++;
+			}
+		}
+		while(i<n){
+			ans[k]=A[i];
+			i++;
+			k++;
+		}
+		while(j<m){
+			ans[k]=B[j];
+			j++;
+			k++;
+		}
+		return ans;
+	}
 
 }
